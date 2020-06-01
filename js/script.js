2a054a8bc6bac774f0a46742d64c6cfc5d5ef924
ZloiGoroh <cha4anov@gmail.com>
2020-06-01 21:26:26 +0500

    Finished making grid on the desk

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
+var grid = function(){
+	let grid = '';
+	let isWhite = true;
+	let bgColor = '';
+	for (let i = 0; i < 8; ++i){
+		for (let j = 0; j < 8; ++j){
+			if (isWhite){
+				bgcolor = 'white';
+			} else {
+				bgcolor = 'black';
+			}
+			grid += `<div class="${bgcolor}"></div>`
+			isWhite = !isWhite;
+		}
+		isWhite = !isWhite;
+	}
+	return grid;
+}
+
 var game = new Vue({
 	el:'#chess',
 	data: {
-		
+		grid: grid()	
 	},
 	methods: {
 		
 	}
 });
+
+
