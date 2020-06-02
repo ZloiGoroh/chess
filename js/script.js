228b966a284adc3cc648a9451efc5cdf388588ac
ZloiGoroh <cha4anov@gmail.com>
2020-06-03 02:15:51 +0500

    Made desk possible to config (range is now variable). Added images for figures. Made JSON file with list of figures. Added comments.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,48 @@
+var deskRange = 8;      //How big should be the desk
+var whiteSide = [];     //Array of white figures
+var blackSide = [];     //Array of black figures
+var deskFields = [];    //Matrix of the desk
+
+
+
+class Fields {
+	constructor(){
+		isFull = false;
+	}
+
+}
+
+class Figures{
+        constructor(name, src, range, color, dirs, moveX, moveY){
+                this.name = name;
+                this.texture = src;
+                this.range = range;
+                this.isWhite = color;
+                this.direction = dirs;
+                this.isAttacked = false;
+                this.posX = moveX;
+                this.posY = moveY;
+                this.isMoved = false;
+        }
+        moveFigure(){
+                
+        }
+}
+
+        //Function, that creates the desk, gives ids and background colour to fields
+
 var grid = function(){
 	let grid = '';
 	let isWhite = true;
 	let bgColor = '';
-	for (let i = 0; i < 8; ++i){
-		for (let j = 0; j < 8; ++j){
+	for (let i = deskRange; i > 0; --i){
+		for (let j = 0; j < deskRange; ++j){
 			if (isWhite){
 				bgcolor = 'white';
 			} else {
 				bgcolor = 'black';
 			}
-			grid += `<div class="${bgcolor}"></div>`
+			grid += `<div class="${bgcolor}" id="${10 *(j + 1) + i}"></div>`
 			isWhite = !isWhite;
 		}
 		isWhite = !isWhite;
@@ -20,7 +53,9 @@ var grid = function(){
 var game = new Vue({
 	el:'#chess',
 	data: {
-		grid: grid()	
+		grid: grid(),	
+                range: `repeat(${deskRange}, 1fr)`
+
 	},
 	methods: {
 		
